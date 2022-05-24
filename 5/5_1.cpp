639234f89cdf45cc6d093d677d89562f333431c1
Pavel <paveldm03@gmail.com>
2022-05-24 21:19:05 +0300

    Update 5_1.cpp

--- 5/5_1.cpp
+++ 5/5_1.cpp
@@ -7,6 +7,7 @@ struct node {
 };
 
 void creat_list(node*& L, int n) {
+	cout << "Введите числа: ";
 	node* q, *q1;
 	for (int i = 1; i <= n; i++) {
 		q = new node;
@@ -25,24 +26,46 @@ void creat_list(node*& L, int n) {
 	}
 }
 
-bool same_elem(node* L) {
-	while (L->next) {
-		node* tec = L->next;
-		while (tec) {
-			if (tec->data == L->data) {
-				return true;
-			}
-			L = L->next;
-		}
-		return false;
-	}
-}
 void print_list(node* L) {
+	cout << "Вывод списка: ";
 	node* q = L;
 	while (q != NULL) {
 		cout << q->data << " ";
 		q = q->next;
 	}
+	cout << "\n";
+}
+
+bool same_elem(node* L) {
+	node* q = L;
+	while (q->next != NULL) {
+		while (q->next != NULL) {
+			q = q->next;
+			if (L->data == q->data) {
+				return true;
+			}
+		}
+	}
+}
+
+struct node* find_max(node* L) {
+	node* q = L;
+	for (; q != NULL; q = q->next) {
+		if (q->data > L->data) {
+			L = q;
+		}
+	}
+	return L;
+}
+
+struct node * del_max(node* L, node* del) {
+	node* q = L;
+	while (q->next != del) {
+		q = q->next;
+	}
+	q->next = del->next;
+	free(del);
+	return(q);
 }
 
 int main() {
@@ -54,12 +77,15 @@ int main() {
 	cin >> n;
 
 	creat_list(L, n);
+	//b = same_elem(L);
+	//if (b) {
+	//	cout << "В списке есть два одинаковых элемента";
+	//}
+	//else {
+	//	cout << "В списке нет одинаковых элементов";
+	//}
+	cout << "\n";
+	node* max_n = find_max(L);
+	del_max(L, max_n);
 	print_list(L);
-	b = same_elem(L);
-	if (b) {
-		cout << "12";
-	}
-	else {
-		cout << "false";
-	}
 }
